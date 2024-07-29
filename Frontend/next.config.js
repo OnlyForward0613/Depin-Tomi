48dba91b8d663d7680abd2c0ea65772d846dcaf2
OnlyForward0613 <153643788+OnlyForward0613@users.noreply.github.com>
2024-07-29 00:30:04 -0500

    FE-update next config update
    Co-Authored-By: Monte dev <15940133+montedev0516@users.noreply.github.com>

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -2,14 +2,14 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+// await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
     eslint: {
         ignoreDuringBuilds: true
     },
-    basePath: "/localhost:3000"
+    // basePath: "/localhost:3000"
 };
 
 
